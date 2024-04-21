7c2d6212cfa11ab0daebe365bdbbc547c404ad52
Jana Maire <s0563285@htw-berlin.de>
2024-04-21 17:58:56 +0200

    initial commit

---
+++ color-contrast-app/src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import MyList from './components/MyList.vue';
+
+const items = ref(['a', 'b', 'c']);
+const items2 = ref(['d', 'e', 'f']);
+
+const text = ref('');
+
+const add = () => {
+  console.log(text.value);
+  items.value.push(text.value);
+  text.value = '';
+}
+</script>
+
+<template>
+  <div>
+
+    {{ items }}
+
+    <my-list :items="items" />
+
+    <my-list :items="items2" title="List 2" />
+
+    <input type="text" v-model="text">
+    <p>{{ text }}</p>
+    <button type="button" @click="add">Add</button>
+  </div>
+</template>
---
+++ color-contrast-app/src/components/MyList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+interface Props {
+    title?: string;
+    items: string[];
+}
+
+const { items } = withDefaults(defineProps<Props>(), {
+    title: 'My List'
+});
+</script>
+
+<template>
+    <h2>{{ title }}</h2>
+    <ul>
+
+        {{ items }}
+        <li v-for="(item, idx) in items" :key="idx">{{ item }}</li>
+    </ul>
+</template>
